4f836688f7fe4618ec41b99093a55f6a3c3d9551
liurongchan <liurongchan@liurongchan-ThinkPad-X230.(none)>
2014-04-23 11:47:47 +0800

    initialize

---
+++ app/src/main/java/org/qinshuihepan/bbs/App.java
@@ -0,0 +1,39 @@
+package org.qinshuihepan.bbs;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * Created by liurongchan on 14-4-23.
+ */
+public class App extends Application {
+    private static Context sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = getApplicationContext();
+        initImageLoader(getApplicationContext());
+    }
+
+    public static Context getContext() {
+        return sContext;
+    }
+
+    // 初始化ImageLoader
+    public static void initImageLoader(Context context) {
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024)).discCacheSize(20 * 1024 * 1024)
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
+}
---
+++ app/src/main/java/org/qinshuihepan/bbs/dao/DBHelper.java
@@ -0,0 +1,29 @@
+package org.qinshuihepan.bbs.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by liurongchan on 14-4-23.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "uestc.db";
+
+    private static final int VERSION = 1;
+
+    public  DBHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
