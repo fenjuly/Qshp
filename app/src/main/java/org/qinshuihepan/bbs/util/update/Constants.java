c238e900207d76cd5861521b3659441743f1250f
黄璞 <765773404@qq.com>
2014-05-16 09:06:28 +0800

    Update Constants.java

--- app/src/main/java/org/qinshuihepan/bbs/util/update/Constants.java
+++ app/src/main/java/org/qinshuihepan/bbs/util/update/Constants.java
@@ -3,7 +3,7 @@ package org.qinshuihepan.bbs.util.update;
 public class Constants {
 
     //服务器
-	protected static final String APP_UPDATE_SERVER_URL = "http://222.197.183.133:81/wordpress/index.html";
+	protected static final String APP_UPDATE_SERVER_URL = "http://222.197.183.133/development/index.html";
 	
 	// json {"url":"http://192.168.1.115:8080/xxx.apk","versionCode":2,"updateMessage":"版本更新信息"}
 	//服务器返回的json数据格式，根据实际情况修改参数
