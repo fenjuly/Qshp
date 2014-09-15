8726374ddf404e92ae458e75a096dc82beb4babc
callofdutyops <765773404@qq.com>
2014-09-15 17:31:58 +0800

    一些细节修改 登陆界面优化

--- app/src/main/java/org/qinshuihepan/bbs/ui/LaunchActivity.java
+++ app/src/main/java/org/qinshuihepan/bbs/ui/LaunchActivity.java
@@ -6,27 +6,30 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.KeyEvent;
 
 import org.qinshuihepan.bbs.R;
 import org.qinshuihepan.bbs.util.sharedpreference.Athority;
 
 /**
  * Created by liurongchan on 14-5-7.
+ * Modified on 14-9-15
  */
 public class LaunchActivity extends Activity {
 
-    private Context mContext;
+    public static final String PREF_HAS_LOGINED = "has_logined";
 
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
         mContext = this;
-        getActionBar().hide();
-        setContentView(R.layout.launch);
         final SharedPreferences mShared = mContext.getSharedPreferences(
                 Athority.ACCOUNT_INFORMATION, Context.MODE_PRIVATE);
-        if (mShared.getString("login", "no").equals("yes")) {
+        if (mShared.getString(PREF_HAS_LOGINED, "no").equals("yes")) {
             new Handler().postDelayed(new Runnable() {
 
                 @Override
@@ -49,7 +52,11 @@ public class LaunchActivity extends Activity {
                 }
             }, 2000);
         }
-
     }
 
+    //屏蔽所有按键
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return true;
+    }
 }
