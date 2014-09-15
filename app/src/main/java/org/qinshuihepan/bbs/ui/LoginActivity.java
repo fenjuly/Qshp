d3fc1891369fa6c73e1185cb0b2bee81ff8b054f
callofdutyops <765773404@qq.com>
2014-09-16 00:09:14 +0800

    登陆换为mobile的APi

--- app/src/main/java/org/qinshuihepan/bbs/ui/LoginActivity.java
+++ app/src/main/java/org/qinshuihepan/bbs/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,12 +14,9 @@ import com.dd.CircularProgressButton;
 
 import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.qinshuihepan.bbs.R;
 import org.qinshuihepan.bbs.api.Api;
 import org.qinshuihepan.bbs.data.Request;
-import org.qinshuihepan.bbs.util.MD5;
 import org.qinshuihepan.bbs.util.TaskUtils;
 import org.qinshuihepan.bbs.util.sharedpreference.Athority;
 
@@ -70,8 +68,6 @@ public class LoginActivity extends Activity {
                     Toast.makeText(mContext, "密码不能为空！", Toast.LENGTH_SHORT)
                             .show();
                 } else {
-                    MD5 m = new MD5();
-                    password = m.getMD5ofStr(password);
                     confirm.setProgress(50);
                     TaskUtils.executeAsyncTask(new AsyncTask<Void, Void, Boolean>() {
                         @Override
@@ -89,7 +85,6 @@ public class LoginActivity extends Activity {
                                 usernameText.setText("");
                                 passwordText.setText("");
                             } else {
-                                confirm.setProgress(100);
                                 Toast.makeText(mContext, "登录成功!", Toast.LENGTH_SHORT)
                                         .show();
                                 Intent intent = new Intent(mContext, MainActivity.class);
@@ -105,41 +100,31 @@ public class LoginActivity extends Activity {
     }
 
     private boolean isLoginSuccess() {
-        Document doc = null;
-        String formhash = null;
-        String loginaction = null;
         Map<String, String> datas = new HashMap<String, String>();
-        Map<String, String> cookies = new HashMap<String, String>();
         try {
-            Connection.Response before_login = Request.execute(Api.COOKIE_LOGIN, "Mozilla", Connection.Method.POST);
-            doc = before_login.parse();
-            Element hash = doc.getElementById("scbar_form");
-            Elements hashs = hash.getElementsByTag("input");
-            for (Element element : hashs) {
-                if (element.attr("name").equals("formhash")) {
-                    formhash = element.attr("value");
-                }
-            }
+            Connection.Response before_login = Request.execute(Api.MOBILE_COOKIE_LOGIN, Api.USER_AGENT, Connection.Method.GET);
+            Document doc = before_login.parse();
+            String formhash = doc.getElementById("formhash").attr("value");
+            String loginaction = doc.getElementsByTag("form").attr("action");
+            Log.d("loginaction", loginaction);
 
-            Elements login = doc.getElementsByClass("cl");
-            loginaction = login.attr("action");
-            loginaction = loginaction.replace("amp", "");
             datas.put("formhash", formhash);
             datas.put("referer", Api.HOST);
-            datas.put("loginfield", "username");
+            datas.put("fastloginfield", "username");
+            datas.put("username", username);
             datas.put("password", password);
             datas.put("questionid", "0");
-            datas.put("loginsubmit", "true");
-            datas.put("username", username);
-            cookies = before_login.cookies();
-            cookies.put("sendmail", "1");
-            Connection.Response logining = Request.execute(String.format(Api.LOGIN, loginaction), "Mozilla", datas, cookies, Connection.Method.POST);
+            datas.put("submit", "登陆");
+            datas.put("answer", "");
+
+            Map<String, String> cookies = before_login.cookies();
+            Connection.Response logining = Request.execute(Api.HOST + loginaction, Api.USER_AGENT, datas, cookies, Connection.Method.POST);
             if (logining.cookie("v3hW_2132_auth") == null) {
                 return false;
             } else {
                 cookies = logining.cookies();
                 cookies.put("v3hW_2132_saltkey", before_login.cookie("v3hW_2132_saltkey"));
-                cookies.put("login", "yes");
+                cookies.put(Athority.PREF_HAS_LOGINED, "yes");
                 Athority.addCookies(cookies);
                 return true;
             }
@@ -148,5 +133,4 @@ public class LoginActivity extends Activity {
         }
         return false;
     }
-
 }
