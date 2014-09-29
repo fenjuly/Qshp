1b6e5f912c0b816fa182afb328dc5e0c99f4d558
callofdutyops <765773404@qq.com>
2014-09-29 21:50:37 +0800

    complete update function

--- app/src/main/java/org/qinshuihepan/bbs/util/update/Constants.java
+++ app/src/main/java/org/qinshuihepan/bbs/util/update/Constants.java
@@ -8,6 +8,5 @@ public class Constants {
     public static final String APK_UPDATE_CONTENT = "updateMessage";
     public static final String APK_VERSION_CODE = "versionCode";
     //服务器
-    protected static final String APP_UPDATE_SERVER_URL = "http://222.197.183.133/development/index.html";
-
+    protected static final String APP_UPDATE_SERVER_URL = "http://hp.stuhome.net/development/index.html";
 }
